04daddedfe9c1a9603816dbf393bba1d5224b301
itzelserrano <124410725+itzelserrano@users.noreply.github.com>
2023-06-15 18:58:15 -0700

    15/junio/2023

--- parcial3/practica7.php
+++ parcial3/practica7.php
@@ -14,14 +14,15 @@
 </head>
 <body>
    <?php
-    $colores=["#037AF8", "#7FB6F0", "#7FE2F0  ", "#159EB0 ", " #1567B0 ", "#D1E6F9 
-    ", "", "  ", " ", ];
+    $colores=["#037AF8", "#7FB6F0", "#7FE2F0  ", "#159EB0 ", " #1567B0 ", "#D1E6F9 ", ];
    ?>
     <div class="container">
         <h1>Practica 7- Tablero de serpientes y escaleras</h1>
         <hr>
+        
 
         <form action="">
+            <input type="submit" value="Jugar" class="btn btn-success"><hr>
         <div class="row">
             <?php
                 for($i=100; $i>0; $i--){
